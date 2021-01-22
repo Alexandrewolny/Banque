03110482b8105985b04a6316f213c472612d4d0a
alexandrewolny <alexandrewolny>
2021-01-22 16:10:36 +0100

    Création du Create&Read dans CompteController

--- src/main/java/com/example/demo/controllers/CompteController.java
+++ src/main/java/com/example/demo/controllers/CompteController.java
@@ -3,6 +3,10 @@ package com.example.demo.controllers;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +18,25 @@ public class CompteController {
 
 	private List<Compte> comptes = initCompte();
 	
+	//READ
+	@GetMapping() //localhost:8080/comptes --> GET
+	public List<Compte> findAll() {
+		return this.comptes;
+	}
+	
+	@GetMapping("/{id}") //localhost:8080/comptes/id --> GET
+	public Compte findById(@PathVariable int id) {
+		return this.comptes.get(id);
+	}
+	
+	//CREATE
+	@PutMapping("/{id}") //localhost:8080/id --> PUT
+	public Compte udpate(@PathVariable int id,@RequestBody Compte compte) {
+		Compte newCompte = findById(id);
+		newCompte.setNumero(compte.getNumero());
+		newCompte.setSolde(compte.getSolde());
+		return newCompte;
+	}
 	
 	/**
 	 * Methode qui creer et retourne une liste instanciée de comptes
@@ -24,7 +47,7 @@ public class CompteController {
 		comptes.add(new Compte(1,450.78));
 		comptes.add(new Compte(2,740.25));
 		comptes.add(new Compte(3,1250.32));
-		return comptes;
+		return comptes ;
 		
 	}
 }
